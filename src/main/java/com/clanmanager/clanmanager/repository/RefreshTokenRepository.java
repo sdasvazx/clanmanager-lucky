ef599e51bd106fe83e2eb68e6a8f578ce1271fd3
hyun1127 <qkrgus825@gmail.com>
2026-08-10 00:18:39 +0900

    Add refresh token repository

---
+++ src/main/java/com/clanmanager/clanmanager/repository/RefreshTokenRepository.java
@@ -0,0 +1,10 @@
+package com.clanmanager.clanmanager.repository;
+
+import com.clanmanager.clanmanager.entity.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByTokenHash(String tokenHash);
+}
